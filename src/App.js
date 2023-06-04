2e3dadda1124d5443e82609b69c456ac4173d41e
Anh Truong Nguyen <truongnguyen4042@gmail.com>
2023-06-04 21:21:56 +1000

    create add note

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useReducer } from 'react';
 import { API } from 'aws-amplify';
-import { List } from 'antd';
-//import 'antd/dist/antd.css'; // Import the overall Ant Design styles
+import { List, Button, Input } from 'antd';
 import 'antd/lib/button/style'; // Import the styles for the Button component
 import 'antd/lib/input/style'; // Import the styles for the Input component
 import { listNotes } from './graphql/queries';
+import { v4 as uuid } from 'uuid'; // Import the UUID library to create a unique identifier for the client
+import { createNote as CreateNote } from './graphql/mutations'; // Import the createNote mutation definition
+
+// reate a new CLIENT_ID variable
+const CLIENT_ID = uuid();
 
 const initialState = {
   notes: [],
@@ -15,10 +19,16 @@ const initialState = {
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'SET_NOTES':
+    case 'SET_NOTES': // List all notes from listNotes
       return { ...state, notes: action.notes, loading: false }
     case 'ERROR':
       return { ...state, loading: false, error: true }
+    case 'ADD_NOTE': // adding a new note to the loal state
+      return { ...state, notes: [action.note, ...state.notes] }
+    case 'RESET_FORM': // resetting the form state to clear out the form
+      return { ...state, form: initialState.form }
+    case 'SET_INPUT': // updating the form state when the user types
+      return { ...state, form: { ...state.form, [action.name]: action.value } }
     default:
       return state
   }
@@ -39,6 +49,31 @@ function App() {
       console.log('error: ', err);
       dispatch({ type: 'ERROR' })
     }
+  }
+
+  // create the createNote function
+  async function createNote() {
+    const { form } = state;
+    if (!form.name || !form.description) {
+      return alert('please enter a name and ddescription!')
+    }
+    const note = { ...form, clientId: CLIENT_ID, completed: false, id: uuid() }
+    dispatch({ type: 'ADD_NOTE', note })
+    dispatch({ type: 'RESET_FORM' })
+    try {
+      await API.graphql({
+        query: CreateNote,
+        variables: { input: note }
+      })
+      console.log("note created");
+    } catch (err) {
+      console.log("error: ", err);
+    }
+  }
+
+  // onChange handler to update the form state when user interacts with an input
+  function onChange(e) {
+    dispatch({ type: 'SET_INPUT', name: e.target.name, value: e.target.value })
   }
 
   const styles = {
@@ -65,13 +100,33 @@ function App() {
 
 
   return (
-
-    <div style={styles.container}>
-      <List
-        loading={state.loading}
-        dataSource={state.notes}
-        renderItem={renderItem}
+    <div>
+      <Input
+        onChange={onChange}
+        value={state.form.name}
+        placeholder='Note name'
+        name='name'
+        style={styles.input}
       />
+      <Input
+        onChange={onChange}
+        value={state.form.description}
+        placeholder='Description'
+        name='description'
+        style={styles.input}
+      />
+      <Button
+        onClick={createNote}
+        type='primary'
+      >Create Note</Button>
+
+      <div style={styles.container}>
+        <List
+          loading={state.loading}
+          dataSource={state.notes}
+          renderItem={renderItem}
+        />
+      </div>
     </div>
   )
 }
